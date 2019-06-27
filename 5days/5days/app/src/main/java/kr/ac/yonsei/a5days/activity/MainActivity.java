bb5c6112074e34230007632a5ed309a7bb4c1bbe
junseim <junesimtom@gmail.com>
2019-06-27 10:57:05 +0900

    Finished Database

--- 5days/5days/app/src/main/java/kr/ac/yonsei/a5days/activity/MainActivity.java
+++ 5days/5days/app/src/main/java/kr/ac/yonsei/a5days/activity/MainActivity.java
@@ -1,17 +1,22 @@
 package kr.ac.yonsei.a5days.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.List;
+
 import kr.ac.yonsei.a5days.R;
 import kr.ac.yonsei.a5days.item.Goal;
+import kr.ac.yonsei.a5days.util.DataBase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private boolean check = false;
@@ -21,7 +26,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        DataBase manager = new DataBase(getApplicationContext(),"Goal",null,1);
+        List<String> list = manager.select();
+        TextView text = findViewById(R.id.text2);
+        String str = null;
+        for(int i = 0; i < list.size();i++){
+            str += list.get(i)+"\n";
+        }
+        text.setText(str);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(this);
     }
@@ -56,7 +68,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 @Override
                 public void onPositiveClicked(Goal goal) {
                     TextView t = findViewById(R.id.text2);
-                    t.setText(goal.getName());
+                    DataBase manager = new DataBase(getApplicationContext(),"Goal",null,1);
+                    manager.exequte("INSERT INTO Goal values('"+goal.getName()
+                            +"', '"+goal.getDate()+"', "+goal.getLevel()+", "+goal.getPoint()+")");
                 }
             });
             dialog.show();
